9afa5c9e72a54193bf467a7f8427db8548848696
louick <louickmytc@gmail.com>
2025-06-23 16:10:37 -0300

    ajusta vite.config.js para deploy no GH Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
-  // server: { allowedHosts: [...] } // <-- REMOVIDO!
+  plugins: [react()],
+  base: '/cop30/',   // <-- ESSA LINHA É FUNDAMENTAL!
 })
