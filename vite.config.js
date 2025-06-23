f0e89189603762a48c2a0ae90e3a4cfd0633d514
louick <louickmytc@gmail.com>
2025-06-23 16:02:57 -0300

    vite config ajustado

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: [
-      'd362-2804-d4b-b409-7e00-6cda-8df7-514d-7205.ngrok-free.app'
-    ]
-  }
+  plugins: [react()]
+  // server: { allowedHosts: [...] } // <-- REMOVIDO!
 })
